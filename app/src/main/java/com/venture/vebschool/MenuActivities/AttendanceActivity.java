ed637ebcd0df446249660896e8d74bbca8dd3a6b
Venture <jeenareena2016@gmail.com>
2019-06-13 10:59:56 +0530

    11/06/2019 update

--- app/src/main/java/com/venture/vebschool/MenuActivities/AttendanceActivity.java
+++ app/src/main/java/com/venture/vebschool/MenuActivities/AttendanceActivity.java
@@ -1,8 +1,10 @@
 package com.venture.vebschool.MenuActivities;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.venture.vebschool.Home;
 import com.venture.vebschool.R;
 
 public class AttendanceActivity extends AppCompatActivity {
@@ -11,5 +13,10 @@ public class AttendanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+
+        /*Dialog dialog=new Dialog(AttendanceActivity.this);
+        dialog.setContentView(R.layout.attendance_details);
+        dialog.show();
+       */
     }
 }
